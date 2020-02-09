bf5fb9479840573bc4a55b642c0ccd3e5bf2f2d6
Gabe Young <gabriel_young@emerson.edu>
2020-02-09 16:44:28 -0500

    Improved drop down/button creation

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,28 +9,35 @@ var lowCarb= $("<button>Low Carbs</button>")
 var lowCal=$("<button>Low Cal</button>")
 var lowFat=$("<button>Low Fat</button>")
 //Heart Disease Buttons
-var cholFree= $("<button>Cholestorl Free</button>");
+var cholFree= $("<button>Cholesterol Free</button>");
 var lowSod = $("<button> Low Sodium</button>");
-var highFiber = $("<button> High Fiber</button");
+var highFiber = $("<button> High Fiber</button>");
 //Food Allergy Buttons
 var milkFree= $("<button>Lactose Intolerant</button>");
 var nutFree= $("<button> Nut Allergy</button>");
 var eggFree= $("<button> Egg Allergy</button>");
 //Set all these buttons to share the same class based on what drop down option they came from. As well as an id for the data pull.
 
-$(lowCarb).attr("class", "queryBtn");
+$(lowCarb).attr("class", "queryBtn btn btn-primary");
 $(lowCarb).attr("id", "low-carb");
-$(lowCal).attr("class", "queryBtn");
+$(lowCal).attr("class", "queryBtn btn btn-primary");
 $(lowCal).attr("id", "low-calorie");
-$(lowFat).attr("class", "queryBtn");
+$(lowFat).attr("class", "queryBtn btn btn-primary");
 $(lowFat).attr("id", "low-fat");
 
-$(cholFree).attr("class", "queryBtn");
+$(cholFree).attr("class", "queryBtn btn btn-primary");
 $(cholFree).attr("id", "cholesterol-free");
-$(lowSod).attr("class", "queryBtn");
+$(lowSod).attr("class", "queryBtn btn btn-primary");
 $(lowSod).attr("id", "low-sodium");
-$(highFiber).attr("class", "queryBtn");
+$(highFiber).attr("class", "queryBtn btn btn-primary");
 $(highFiber).attr("id", "high-fiber");
+
+$(milkFree).attr("class", "queryBtn btn btn-primary");
+$(milkFree).attr("id", "no-milk-ingredients");
+$(nutFree).attr("class", "queryBtn btn btn-primary");
+$(nutFree).attr("id", "no-tree-nut-ingredients");
+$(eggFree).attr("class", "queryBtn btn btn-primary");
+$(eggFree).attr("id", "no-egg-ingredients");
 
 
 
@@ -45,21 +52,35 @@ function jsFunction(m){
     if (m<=1){
         //var r= $("<");
         $("#btnHome").empty();
-        $("#btnHome").append(lowSbtn);
+        $("#btnHome").append(lowCal);
+        $("#btnHome").append("<br>");
         $("#btnHome").append(lowCarb);
+        $("#btnHome").append("<br>");
         $("#btnHome").append(lowFat);
     } else if(m<=2){
         $("#btnHome").empty();
         $("#btnHome").append(cholFree);
+        $("#btnHome").append("<br>");
         $("#btnHome").append(lowSod);
+        $("#btnHome").append("<br>");
+        $("#btnHome").append(highFiber);
     }else if(m=3){
         $("#btnHome").empty();
+        $("#btnHome").append(milkFree);
+        $("#btnHome").append("<br>");
+        $("#btnHome").append(nutFree);
+        $("#btnHome").append("<br>");
+        $("#btnHome").append(eggFree);
     }
 };
 
 //Notes:
-//Must get the btnHome div to clear on value change before new info is appended. 
+
+//style the buttons according to css for buttons
+//Add a home page function that will take you back to the Doctors Page
 //Fill out the rest of the links coresponding buttons 
 //Add functionality to the buttons (communicate with Rashmi about this)
+
+
 
 //Work on wireframe box layout on Monday
