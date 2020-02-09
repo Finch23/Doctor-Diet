a51e8d83575cfcb8eaf366bcce85239c2f98a04b
Rashmi Sharma <rksharma2679@gmail.com>
2020-02-09 15:09:15 -0500

    Adding diet item functionality for low-sodium

---
+++ assets/javascript/dietScript.js
@@ -0,0 +1,41 @@
+var nutriAPIKey = "a9fb6c8edc2b740ab7b9f3d3f9a3eea2";
+var nutriAppId = "49764774";
+var dietEntry = "low-sodium";
+
+var queryNutrition = "https://api.nutritionix.com/v1_1/search/" + dietEntry + "?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=" + nutriAppId + "&appKey=" + nutriAPIKey;
+
+//Ajax call to populate specific diet food-item
+function dietList() {
+    $("#dietList").empty();
+    var queryNutrition = "https://api.nutritionix.com/v1_1/search/" + dietEntry + "?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=" + nutriAppId + "&appKey=" + nutriAPIKey;
+    return $.ajax({
+        url: queryNutrition,
+        method: "GET"
+        }).then(function(res) {
+            for(var i=0; i < 10; i++) {
+                //creating card and setting its content
+                var cardDiv = $("<div>").attr("class","card mb-5 shadow-lg rounded").css("max-width", "18rem");
+                //Creating image and star col
+                var imgIcon = $("<img>").attr("class", "card-img-top m-2").css("width","90%");
+                //Checking the gender of the doctor and displaying the profile icon accordingly
+                if((i%2) === 0) {
+                    imgIcon.attr("src", "assets/images/low-sodium.jpg");
+
+                } else {
+                    imgIcon.attr("src", "assets/images/low-sodium.webp");
+
+                }
+                //Creating card body
+                var cardBodyDiv =$("<div>").attr("class","card-body text-center");
+                // setting body text of card like title, item-name, brand-name and serving size 
+                var cardTitle = $("<p>").attr("class","card-title mb-4 p-2 h5 border-bottom rounded-pill border-info").text(res.hits[i].fields.item_name);
+                var text1 = $("<p>").attr("class", "card-text mb-4").html("<b>Brand Name:</b>" + res.hits[i].fields.brand_name);
+                var text2 = $("<p>").attr("class", "card-text").html("<b>Serving Size:</b> " + res.hits[i].fields.nf_serving_size_qty);
+                cardBodyDiv.append(cardTitle, text1, text2);
+                cardDiv.append(imgIcon, cardBodyDiv);
+                //Adding the Card in the docList div
+                $("#dietList").append(cardDiv);   
+            }
+    });
+}
+
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,20 +1,13 @@
 var betterDocAPIKey = "c570e2001f75afa94675595ad2391528";
 var cityAPIKey = "2a29d28596f5b969578c8d643d9842b5";
-var nutriAPIKey = "a9fb6c8edc2b740ab7b9f3d3f9a3eea2";
-var nutriAppId = "49764774";
-var dietEntry = "low-sodium";
-var primaryDoc = false;
-
-// var queryBetterDoctor = "https://api.betterdoctor.com/2016-03-01/doctors?location=35.732,-78.850,10&limit=10&user_key=c570e2001f75afa94675595ad2391528";
-
-var queryNutrition = "https://api.nutritionix.com/v1_1/search/" + dietEntry + "?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=" + nutriAppId + "&appKey=" + nutriAPIKey;
-//doctorList();
-//dietList();
+var docSpeciality = "";
 
 //function gets triggered when search button is clicked
 function searchDoctor() {
     docArea = $("#searchDoctor").val();
-    if(docArea == 1) primaryDoc = true;
+    if(docArea == 1) docSpeciality = "specialty_uid=internist%2Cfamily-practitioner";
+    if(docArea == 2) 
+    docSpeciality = "specialty_uid=pediatrician%2Callergist%2Cobstetrics-gynecologist%2Cfoot-ankle-orthopedist%2C";
     city =  $("#city").val().trim();
     if($.isNumeric(city) == false){
         //call Ajax call function for city details, five day forecast and add city in the list
@@ -27,7 +20,7 @@ function searchDoctor() {
 //Ajax call to weatherAPI to get the latitude and longitude of a city where the user is searching for doctor
 function cityDetails() {
     var queryCityURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + cityAPIKey;
-    var queryBetterDoctorPart1 = "https://api.betterdoctor.com/2016-03-01/doctors?location=" 
+    var queryBetterDoctorPart1 = "https://api.betterdoctor.com/2016-03-01/doctors?" + docSpeciality + "&location=" 
     var queryBetterDoctorPart2 = ",10&limit=30&user_key=" + betterDocAPIKey;
     $.ajax({
         url: queryCityURL,
@@ -49,67 +42,58 @@ function doctorList(queryBetterDoctor) {
         url: queryBetterDoctor,
         method: "GET"
         }).then(function(res) {
-            for(var i=0; i < 30; i++) {
-                if(res.data[i].specialties[0].name !== 'Dentistry' && res.data[i].specialties[0].name !== 'Ophthalmology') {
-                    if(primaryDoc && (res.data[i].specialties[0].name === 'Family Medicine' || res.data[i].specialties[0].name === 'Internal Medicine')) {
-                        createDocCard(i, res);
-                    } 
-                    if(!primaryDoc && (res.data[i].specialties[0].name !== 'Family Medicine' || res.data[i].specialties[0].name !== 'Internal Medicine')) {
-                        createDocCard(i, res);
-                    }
+            for(var i=0; i < 10; i++) {
+               //creating card and setting its content
+                var cardDiv = $("<div>").attr("class","card mb-2 shadow-lg bg-white rounded").css("max-width", "540px");
+                var cardRow = $("<div>").attr("class","row no-gutters");
+                //Creating image and star col
+                var cardImgCol = $("<div>").attr("class","col-4");
+                var imgIcon = $("<img>").attr("class", "card-img m-2");
+                //Checking the gender of the doctor and displaying the profile icon accordingly
+                if(res.data[i].profile.gender === 'female') {
+                    imgIcon.attr("src", "assets/images/femaleDocImg.jpg");
+
+                } else {
+                    imgIcon.attr("src", "assets/images/maleDocImg.jpg");
+
                 }
+                //Creating star div and span and calling star function to convert numeric rating into stars
+                var starDiv = $("<div>").attr("class","text-center");
+                if (res.data[i].ratings.length !== 0) {
+                    var starSpan = $("<span>").attr("class", "stars").attr("data-rating", res.data[i].ratings[0].rating);
+                    $('.stars').stars();    
+                }
+                //appending star span in star div
+                starDiv.append(starSpan);
+                //Creating Information col of the card
+                var cardInfoCol = $("<div>").attr("class","col-8");
+                var cardBodyDiv =$("<div>").attr("class","card-body text-center");
+                //displaying first and lastname and displaying them as title
+                var cardTitle = $("<p>").attr("class","card-title mb-4 h3").text(res.data[i].profile.first_name + " " +res.data[i].profile.last_name);
+                // setting body text of card like title, speciality, Address and phone number 
+                var text1 = $("<p>").attr("class", "card-text mb-4 h6").text(res.data[i].profile.title);
+                var text2 = $("<p>").attr("class", "card-text").html("<b>Speciality:</b> " + res.data[i].specialties[0].name);
+                var text3 = $("<p>").attr("class", "card-text").html("<b>Address:</b> " + res.data[i].practices[0].visit_address.street + "," + res.data[i].practices[0].visit_address.city);
+                var text4Icon = $("<i>").attr("class", "fa fa-phone-square");
+                var text4Text = " " + res.data[i].practices[0].phones[0].number;
+                var text4 = $("<p>").attr("class", "card-text h5");
+                text4.append(text4Icon, text4Text);
+                //Appending CardTitle and its texts into cardBody
+                cardBodyDiv.append(cardTitle, text1, text2, text3, text4);
+                //Appending cardBody into cardInfoCol
+                cardInfoCol.append(cardBodyDiv);
+                //Appending image into cardImgCol
+                cardImgCol.append(imgIcon, starDiv);
+                ////Appending cardImgCol and cardInfoCol into my cardRow
+                cardRow.append(cardImgCol, cardInfoCol);
+                //finally the cardRow into my Card div
+                cardDiv.append(cardRow);
+                //Adding the Card in the docList div
+                $("#docList").append(cardDiv);
             }
     });
 }
-function createDocCard(i, res) {
-     //creating card and setting its content
-     var cardDiv = $("<div>").attr("class","card mb-2 shadow-lg bg-white rounded").css("max-width", "540px");
-     var cardRow = $("<div>").attr("class","row no-gutters");
-     //Creating image and star col
-     var cardImgCol = $("<div>").attr("class","col-4");
-     var imgIcon = $("<img>").attr("class", "card-img m-2");
-     //Checking the gender of the doctor and displaying the profile icon accordingly
-     if(res.data[i].profile.gender === 'female') {
-         imgIcon.attr("src", "assets/images/femaleDocImg.jpg");
 
-     } else {
-         imgIcon.attr("src", "assets/images/maleDocImg.jpg");
-
-     }
-     //Creating star div and span and calling star function to convert numeric rating into stars
-     var starDiv = $("<div>").attr("class","text-center");
-     if (res.data[i].ratings.length !== 0) {
-         var starSpan = $("<span>").attr("class", "stars").attr("data-rating", res.data[i].ratings[0].rating);
-         $('.stars').stars();    
-     }
-     //appending star span in star div
-     starDiv.append(starSpan);
-     //Creating Information col of the card
-     var cardInfoCol = $("<div>").attr("class","col-8");
-     var cardBodyDiv =$("<div>").attr("class","card-body text-center");
-     //displaying first and lastname and displaying them as title
-     var cardTitle = $("<p>").attr("class","card-title mb-4 h3").text(res.data[i].profile.first_name + " " +res.data[i].profile.last_name);
-     // setting body text of card like title, speciality, Address and phone number 
-     var text1 = $("<p>").attr("class", "card-text mb-4 h6").text(res.data[i].profile.title);
-     var text2 = $("<p>").attr("class", "card-text").html("<b>Speciality:</b> " + res.data[i].specialties[0].name);
-     var text3 = $("<p>").attr("class", "card-text").html("<b>Address:</b> " + res.data[i].practices[0].visit_address.street + "," + res.data[i].practices[0].visit_address.city);
-     var text4Icon = $("<i>").attr("class", "fa fa-phone-square");
-     var text4Text = " " + res.data[i].practices[0].phones[0].number;
-     var text4 = $("<p>").attr("class", "card-text h5");
-     text4.append(text4Icon, text4Text);
-     //Appending CardTitle and its texts into cardBody
-     cardBodyDiv.append(cardTitle, text1, text2, text3, text4);
-     //Appending cardBody into cardInfoCol
-     cardInfoCol.append(cardBodyDiv);
-     //Appending image into cardImgCol
-     cardImgCol.append(imgIcon, starDiv);
-     ////Appending cardImgCol and cardInfoCol into my cardRow
-     cardRow.append(cardImgCol, cardInfoCol);
-     //finally the cardRow into my Card div
-     cardDiv.append(cardRow);
-     //Adding the Card in the docList div
-     $("#docList").append(cardDiv);
-}
 //function to convert numeric rating into star rating
 $.fn.stars = function() {
     return $(this).each(function() {
@@ -125,39 +109,3 @@ $.fn.stars = function() {
         $(this).html(`${fullStar}${halfStar}${noStar}`);
     });
 }
-
-//Ajax call to populate specific diet food-item
-function dietList() {
-    $("#dietList").empty();
-    var queryNutrition = "https://api.nutritionix.com/v1_1/search/" + dietEntry + "?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=" + nutriAppId + "&appKey=" + nutriAPIKey;
-    return $.ajax({
-        url: queryNutrition,
-        method: "GET"
-        }).then(function(res) {
-            for(var i=0; i < 10; i++) {
-                //creating card and setting its content
-                var cardDiv = $("<div>").attr("class","card mb-5 shadow-lg rounded").css("max-width", "18rem");
-                //Creating image and star col
-                var imgIcon = $("<img>").attr("class", "card-img-top m-2").css("width","90%");
-                //Checking the gender of the doctor and displaying the profile icon accordingly
-                if((i%2) === 0) {
-                    imgIcon.attr("src", "assets/images/low-sodium.jpg");
-
-                } else {
-                    imgIcon.attr("src", "assets/images/low-sodium.webp");
-
-                }
-                //Creating card body
-                var cardBodyDiv =$("<div>").attr("class","card-body text-center");
-                // setting body text of card like title, item-name, brand-name and serving size 
-                var cardTitle = $("<p>").attr("class","card-title mb-4 p-2 h5 border-bottom rounded-pill border-info").text(res.hits[i].fields.item_name);
-                var text1 = $("<p>").attr("class", "card-text mb-4").html("<b>Brand Name:</b>" + res.hits[i].fields.brand_name);
-                var text2 = $("<p>").attr("class", "card-text").html("<b>Serving Size:</b> " + res.hits[i].fields.nf_serving_size_qty);
-                cardBodyDiv.append(cardTitle, text1, text2);
-                cardDiv.append(imgIcon, cardBodyDiv);
-                //Adding the Card in the docList div
-                $("#dietList").append(cardDiv);   
-            }
-    });
-}
-
