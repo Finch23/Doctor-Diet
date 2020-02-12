bdf6576865dbbbd969229968e3f7d3484a4bcd47
Gabe Young <gabriel_young@emerson.edu>
2020-02-11 19:23:20 -0500

    updated page/button/API connectivety

--- assets/javascript/dietScript.js
+++ assets/javascript/dietScript.js
@@ -1,93 +1,75 @@
-var betterDocAPIKey = "c570e2001f75afa94675595ad2391528";
-var nutriAPIKey = "";
-var queryBetterDoctor = "https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=" + betterDocAPIKey;
-var btnHouse = $("btnHome");
-
-//various kinds of buttons
-//Diabetes buttons
-var lowCarb= $("<button>Low Carbs</button>")
-var lowCal=$("<button>Low Cal</button>")
-var lowFat=$("<button>Low Fat</button>")
-//Heart Disease Buttons
-var cholFree= $("<button>Cholesterol Free</button>");
-var lowSod = $("<button> Low Sodium</button>");
-var highFiber = $("<button> High Fiber</button>");
-//Food Allergy Buttons
-var milkFree= $("<button>Lactose Intolerant</button>");
-var nutFree= $("<button> Nut Allergy</button>");
-var eggFree= $("<button> Egg Allergy</button>");
-//Set all these buttons to share the same class based on what drop down option they came from. As well as an id for the data pull.
-
-$(lowCarb).attr("class", "queryBtn btn btn-primary");
-$(lowCarb).attr("id", "low-carb");
-$(lowCal).attr("class", "queryBtn btn btn-primary");
-$(lowCal).attr("id", "low-calorie");
-$(lowFat).attr("class", "queryBtn btn btn-primary");
-$(lowFat).attr("id", "low-fat");
-
-$(cholFree).attr("class", "queryBtn btn btn-primary");
-$(cholFree).attr("id", "cholesterol-free");
-$(lowSod).attr("class", "queryBtn btn btn-primary");
-$(lowSod).attr("id", "low-sodium");
-$(highFiber).attr("class", "queryBtn btn btn-primary");
-$(highFiber).attr("id", "high-fiber");
-
-$(milkFree).attr("class", "queryBtn btn btn-primary");
-$(milkFree).attr("id", "no-milk-ingredients");
-$(nutFree).attr("class", "queryBtn btn btn-primary");
-$(nutFree).attr("id", "no-tree-nut-ingredients");
-$(eggFree).attr("class", "queryBtn btn btn-primary");
-$(eggFree).attr("id", "no-egg-ingredients");
-
-
-
-function diet(){
-    location.replace('file:///C:/Users/Gabe/Desktop/Class_Stuff/Project%20%231/Project1/Nutrition.html')
-};
-
-function goDoctors(){
-    location.replace('file:///C:/Users/Gabe/Desktop/Class_Stuff/Project%20%231/Project1/landing.html')
-};
-
-function jsFunction(m){
-    console.log(m);
-    $("#btnHome").innerHTML="";
-    //$("#btnHome").html("");
-    if (m<=1){
-        //var r= $("<");
-        $("#btnHome").empty();
-        $("#btnHome").append(lowCal);
-        $("#btnHome").append("<br>");
-        $("#btnHome").append(lowCarb);
-        $("#btnHome").append("<br>");
-        $("#btnHome").append(lowFat);
-    } else if(m<=2){
-        $("#btnHome").empty();
-        $("#btnHome").append(cholFree);
-        $("#btnHome").append("<br>");
-        $("#btnHome").append(lowSod);
-        $("#btnHome").append("<br>");
-        $("#btnHome").append(highFiber);
-    }else if(m=3){
-        $("#btnHome").empty();
-        $("#btnHome").append(milkFree);
-        $("#btnHome").append("<br>");
-        $("#btnHome").append(nutFree);
-        $("#btnHome").append("<br>");
-        $("#btnHome").append(eggFree);
+//API key and App id of Nutrinix
+var nutriAPIKey = "a9fb6c8edc2b740ab7b9f3d3f9a3eea2";
+var nutriAppId = "49764774";
+var dietEntry = "low-sodium";
+//Rendering Specific diet buttons depending on user's input condition
+function renderDietBtns() {
+    $("#btnHome").html("");
+    console.log("it works");
+    var userCondition = $("#userCondition").val();
+    if(userCondition == 1) {
+       //create buttons for "low-fat, low-calorie, low-carb"  
+       var btn1 = $("<button>").attr("class", "border btn btn-lg mb-2 text-center").attr('onClick', 'openClickedDiet(id)').attr('id', "low-fat").css('display','block').text("Low Fat"); 
+       var btn2 = $("<button>").attr("class", "border btn btn-lg mb-2 text-center").attr('onClick', 'openClickedDiet(id)').attr('id', "low-calorie").css('display','block').text("Low Calorie"); 
+       var btn3 = $("<button>").attr("class", "border btn btn-lg mb-2 text-center").attr('onClick', 'openClickedDiet(id)').attr('id', "low-carb").css('display','block').text("Low Carb");                           
+    } else if(userCondition == 2) {
+        //create buttons for "cholesterol-free, low-sodium, high-fiber"
+        var btn1 = $("<button>").attr("class", "border btn btn-lg mb-2 text-center").attr('onClick', 'openClickedDiet(id)').attr('id', "cholesterol-free").css('display','block').text("Cholesterol Free"); 
+        var btn2 = $("<button>").attr("class", "border btn btn-lg mb-2 text-center").attr('onClick', 'openClickedDiet(id)').attr('id', "low-sodium").css('display','block').text("Low Sodium"); 
+        var btn3 = $("<button>").attr("class", "border btn btn-lg mb-2 text-center").attr('onClick', 'openClickedDiet(id)').attr('id', "high-fiber").css('display','block').text("High Fiber"); 
+    } else if(userCondition == 3) {
+        //create buttons for "no-milk-ingredients, no-tree-nut-ingredients, no-egg-ingredients, no-peanut-ingredients"
+        var btn1 = $("<button>").attr("class", "border btn btn-lg mb-2 text-center").attr('onClick', 'openClickedDiet(id)').attr('id', "no-milk-ingredients").css('display','block').text("Dairy Free"); 
+        var btn2 = $("<button>").attr("class", "border btn btn-lg mb-2 text-center").attr('onClick', 'openClickedDiet(id)').attr('id', "no-egg-ingredients").css('display','block').text("Egg Free"); 
+        var btn3 = $("<button>").attr("class", "border btn btn-lg mb-2 text-center").attr('onClick', 'openClickedDiet(id)').attr('id', "no-peanut-ingredients").css('display','block').text("Peanut Free");
     }
-};
+    $("#btnHome").append(btn1, btn2, btn3);
+}
 
-//Notes:
-
-//style the buttons according to css for buttons
-//Add a home page function that will take you back to the Doctors Page
-//Add functionality to the buttons (communicate with Rashmi about this)
-
-
-
-//Work on wireframe box layout on Monday
-
-function goDoctors(){
+//function when a city is clicked on search history
+function openClickedDiet(id){
+    dietEntry = id;
+    dietList();
+}
+//Ajax call to populate specific diet food-item
+function dietList() {
+    $("#docImage").hide();
+    $(".dietList1").empty();
+    $(".dietList2").empty();
+    $(".dietList3").empty();
+    var queryNutrition = "https://api.nutritionix.com/v1_1/search/" + dietEntry + "?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=" + nutriAppId + "&appKey=" + nutriAPIKey;
+    return $.ajax({
+        url: queryNutrition,
+        method: "GET"
+        }).then(function(res) {
+            for(var i=0; i < 9; i++) {
+                //creating card and setting its content
+                var cardDiv = $("<div>").attr("class","card mb-2 shadow-lg rounded").css("max-width", "15rem");
+                //Creating image and star col
+                var imgIcon = $("<img>").attr("class", "card-img-top").css("width","90%");
+                //Checking the gender of the doctor and displaying the profile icon accordingly
+                if((i%2) === 0) {
+                    imgIcon.attr("src", "assets/images/low-sodium.jpg");
+                } else {
+                    imgIcon.attr("src", "assets/images/low-sodium.webp");
+                }
+                //Creating card body
+                var cardBodyDiv =$("<div>").attr("class","card-body text-center");
+                // setting body text of card like title, item-name, brand-name and serving size 
+                var cardTitle = $("<p>").attr("class","card-title mb-2 p-2 h5 border-bottom rounded-pill border-info").text(res.hits[i].fields.item_name);
+                var text1 = $("<p>").attr("class", "card-text mb-2").html("<b>Brand Name:</b>" + res.hits[i].fields.brand_name);
+                var text2 = $("<p>").attr("class", "card-text").html("<b>Serving Size:</b> " + res.hits[i].fields.nf_serving_size_qty);
+                cardBodyDiv.append(cardTitle, text1, text2);
+                cardDiv.append(imgIcon, cardBodyDiv);
+                //Adding the Card in the docList div
+                if(i < 3) {
+                    $(".dietList1").append(cardDiv);
+                }else if(i < 6) {
+                    $(".dietList2").append(cardDiv);
+                }else if(i < 9) 
+                    $(".dietList3").append(cardDiv);   
+            }
+    });
+}
 
 }
